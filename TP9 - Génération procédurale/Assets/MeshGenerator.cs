9ad8fcced60a7a70d02dcdd7b810acc58ce0febb
NathanRaki <nathan.raklure@gmail.com>
2021-11-16 16:22:58 +0100

    Added Secondary Roads & Bezier Curves

--- TP9 - Génération procédurale/Assets/MeshGenerator.cs
+++ TP9 - Génération procédurale/Assets/MeshGenerator.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
@@ -8,7 +7,7 @@ public class MeshGenerator : MonoBehaviour
 
     private Mesh _mesh;
 
-    private Vector3[] _vertices;
+    public Vector3[] _vertices;
     private int[] _triangles;
     private Color[] _colors;
 
@@ -25,6 +24,8 @@ public class MeshGenerator : MonoBehaviour
 
     public Graphe G;
 
+    List<List<int>> Roads;
+
 
     public class Graphe
     {
@@ -38,17 +39,70 @@ public class MeshGenerator : MonoBehaviour
         }
     }
 
-    private List<int> GenerateRoad(int start, int end)
+    private List<int> GenerateRoad(int type, List<int> ParentRoad = null)
     {
-        int[] pred = Dijkstra(start);
-        int cursor = end;
         List<int> Road = new List<int>();
-        while (cursor != -1)
+        List<int> Temp = new List<int>();
+        // Main Road
+        if (type == 1)
         {
-            Road.Add(cursor);
-            cursor = pred[cursor];
+            for (int i = 0; i < XSize+1; i++)
+            {
+                int[] pred = Dijkstra(i);
+                for (int j = (XSize+1) * ZSize; j < (XSize + 1) * (ZSize + 1); j++)
+                {
+                    int cursor = j;
+                    Temp.Clear();
+                    while (cursor != -1)
+                    {
+                        Temp.Add(cursor);
+                        cursor = pred[cursor];
+                    }
+                    if (Temp.Count > Road.Count)
+                    {
+                        Road = new List<int>(Temp);
+                        Road.Reverse();
+                    }
+                }
+            }
         }
-        Road.Reverse();
+        // Secondary Road
+        else if (type == 2)
+        {
+            int start = ParentRoad[Random.Range(0, ParentRoad.Count)];
+            int[] pred = Dijkstra(start);
+            int width = 10;
+            for (int i = -width; i <= width; i++)
+            {
+                for (int j = -width; j <= width; j++)
+                {
+                    if (i==0 && j==0) continue;
+                    int end = start + i * (XSize+1) + j;
+                    if (end < 0 || end >= (XSize+1)*(ZSize+1)) continue;
+                    Temp.Clear();
+                    while (end != -1)
+                    {
+                        Temp.Add(end);
+                        end = pred[end];
+                    }
+                    if (Temp.Count > Road.Count && Temp.Count > 1 && Temp.Count < 10)
+                    {
+                        Road = new List<int>(Temp);
+                        Road.Reverse();
+                    }
+                }
+            }
+        }
+        // Tertiary Road
+        else if (type == 3)
+        {
+
+        }
+        for (int i = 0; i < Road.Count-1; i++)
+        {
+            G.adjMatrix[Road[i]][Road[i + 1]] = 0;
+            G.adjMatrix[Road[i + 1]][Road[i]] = 0;
+        }
         return Road;
     }
 
@@ -131,7 +185,7 @@ public class MeshGenerator : MonoBehaviour
 
             for (int i = 0; i < (XSize + 1) * (ZSize + 1); ++i)
             {
-                if (!poids.Contains(i) && G.adjMatrix[cursor][i] != 0)
+                if (!poids.Contains(i) && G.adjMatrix[cursor][i] > 0)
                 {
                     if (distance[i] > distance[cursor] + G.adjMatrix[cursor][i])
                     {
@@ -144,7 +198,19 @@ public class MeshGenerator : MonoBehaviour
         return pred;
     }
 
-
+    public void GenerateRoads(int n)
+    {
+        GenerateGraphe();
+        Roads.Clear();
+        List<int> Road = GenerateRoad(1);
+        Roads.Add(new List<int>(Road));
+        for (int i = 0; i < n; i++)
+        {
+            Road = GenerateRoad(2, Roads[0]);
+            if (Road.Count > 0)
+                Roads.Add(new List<int>(Road));
+        }
+    }
 
     void Start()
     {
@@ -156,7 +222,20 @@ public class MeshGenerator : MonoBehaviour
         UpdateMesh();
         GenerateGraphe();
         //GetComponent<RouteGenerator>().InitVertices(_vertices);
-        
+        Roads = new List<List<int>>();
+        GenerateRoads(10);
+
+        List<Vector3[]> nodes = new List<Vector3[]>();
+        for (int i = 0; i < Roads.Count; i++)
+        {
+            List<Vector3> road = new List<Vector3>();
+            for (int j = 0; j < Roads[i].Count; j++)
+            {
+                road.Add(_vertices[Roads[i][j]]);
+            }
+            nodes.Add(road.ToArray());
+        }
+        GetComponent<Bernstein>().nodes = nodes;
     }
 
     void CreateShape()
@@ -227,7 +306,7 @@ public class MeshGenerator : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (_vertices == null) return;
+        if (_vertices == null || Roads == null) return;
 
         for (int i = 0; i < _vertices.Length; i++)
         {
@@ -251,7 +330,12 @@ public class MeshGenerator : MonoBehaviour
         //        }
         //    }
         //}
-        //Gizmos.color = Color.blue;
+
+        for (int i = 1; i < Roads.Count; i++)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(_vertices[Roads[i][0]], .1f);
+        }
 
         //List<int> road2 = GenerateRoad(49, 32);
         //for (int i = 0; i < road2.Count - 1; i++)
@@ -265,15 +349,22 @@ public class MeshGenerator : MonoBehaviour
         //    Gizmos.DrawLine(G.vertices[road3[i]], G.vertices[road3[i + 1]]);
         //}
 
-        Gizmos.color = Color.red;
+        //Gizmos.color = Color.red;
 
-        List<int> road1 = GenerateRoad(3, 118);
-        for (int i = 0; i < road1.Count - 1; i++)
-        {
-            Gizmos.DrawLine(G.vertices[road1[i]] + Vector3.up * 0.1f, G.vertices[road1[i + 1]] + Vector3.up * 0.1f);
-        }
+        //for (int i = 0; i < Roads.Count; i++)
+        //{
+        //    if (Roads[i].Count == 0) continue;
+        //    switch (i)
+        //    {
+        //        case 0: Gizmos.color = Color.red; break;
+        //        default: Gizmos.color = Color.blue; break;
+        //    }
+        //    for (int j = 0; j < Roads[i].Count - 1; j++)
+        //    {
+        //        Gizmos.DrawLine(G.vertices[Roads[i][j]] + Vector3.up * 0.1f, G.vertices[Roads[i][j + 1]] + Vector3.up * 0.1f);
+        //    }
+        //}
 
-        
     }
 
 }
